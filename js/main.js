16ce55fe2fc30155792c10a890f71f83d89a5916
Joseba Merino <yosiunico@gmail.com>
2020-02-06 23:41:40 +0100

    Slow typing

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+let cosas = [ 'programador de Angular 2', 'Joseba Merino Pina', 'muy trabajador', 'programador de Java', 'bastante majo'];
+
+let numero = Math.round(Math.random() * (cosas.length - 1));
+console.log(numero);
+slowText(cosas[numero]);
 
 cambiaColor();
 
@@ -8,3 +13,23 @@ function cambiaColor() {
 
 
 }
+
+function slowText(cosa) {
+  let mensaje = 'Hola, soy ' + cosa;
+  let saludoEL = document.getElementById('saludo');
+  unTrozo(saludoEL, mensaje);
+}
+
+function unTrozo(saludoEL, mensaje) {
+  if(mensaje.substring(0,1) === ' '){
+    cambiaColor()
+  }
+  saludoEL.innerHTML += mensaje.substring(0,1) ;
+  if(mensaje.substring(1) === '') {
+    return;
+  } else {
+    setTimeout(() => unTrozo(saludoEL, mensaje.substring(1)), 100)
+  }
+}
+
+
